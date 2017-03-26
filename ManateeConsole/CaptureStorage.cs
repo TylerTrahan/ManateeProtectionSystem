62716525606cb2ec78378622416d2070c5771736
maxibrain <mkostyrkin@gmail.com>
2017-03-27 01:45:11 +0300

    EF removed.

--- ManateeConsole/CaptureStorage.cs
+++ ManateeConsole/CaptureStorage.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Configuration;
+using System.Data.Common;
 using System.Data.Entity;
 using System.Data.SQLite;
+using System.Data.SQLite.Linq;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -12,7 +15,6 @@ using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using Emgu.CV;
-using SQLite.CodeFirst;
 
 namespace ManateeConsole
 {
@@ -21,7 +23,7 @@ namespace ManateeConsole
         private static readonly Lazy<CaptureStorage> LazyInstance = new Lazy<CaptureStorage>(() => new CaptureStorage());
         public static CaptureStorage Instance { get { return LazyInstance.Value; } }
 
-        private readonly CaptureStorageContext _db;
+        private readonly DbConnection _db;
         private readonly object _dbLock = new object();
 
         public string ImageFolder
@@ -30,11 +32,29 @@ namespace ManateeConsole
             set { _imageFolderInfo = string.IsNullOrEmpty(value) ? null : new DirectoryInfo(value); }
         }
 
-        private DirectoryInfo _imageFolderInfo = new DirectoryInfo(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "capture"));
+        private DirectoryInfo _imageFolderInfo;
 
         private CaptureStorage()
         {
-            _db = new CaptureStorageContext();
+            ImageFolder = ConfigurationManager.AppSettings["CaptureStorageFolder"];
+            try
+            {
+                _db = SQLiteProviderFactory.Instance.CreateConnection();
+                Debug.Assert(_db != null);
+                _db.ConnectionString = ConfigurationManager.ConnectionStrings["CaptureStorageDB"].ConnectionString;
+                _db.Open();
+                using (var command = _db.CreateCommand())
+                {
+                    command.CommandText =
+                        "CREATE TABLE IF NOT EXISTS CaptureEntries ([Id] integer PRIMARY KEY AUTOINCREMENT, [Timestamp] datetime NOT NULL, [GateState] int NOT NULL, [Source] int NOT NULL, [FilePath] nvarchar);" +
+                        "CREATE INDEX IF NOT EXISTS IX_Captures_Timestamp ON CaptureEntries (Timestamp);";
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new CaptureStorageException("Failed to initialize DB", e);
+            }
         }
 
         public void AddCapture(IImage image, CaptureSource source, GateState state = GateState.Open, DateTime? timestamp = null)
@@ -46,13 +66,17 @@ namespace ManateeConsole
                 Source = source,
                 FilePath = SaveImage(image, source.ToString()),
             };
-            _db.CaptureEntries.Add(capture);
 
+            var command = _db.CreateCommand();
+            command.CommandText =
+                string.Format(
+                    "INSERT INTO CaptureEntries (Timestamp, GateState, Source, FilePath) VALUES ('{0}Z', {1}, {2}, '{3}')",
+                    capture.Timestamp.ToString("yyyy-MM-dd HH:mm:ss.fffffff"), (int)capture.GateState, (int)capture.Source, capture.FilePath);
             try
             {
                 lock (_dbLock)
                 {
-                    _db.SaveChanges();
+                    command.ExecuteNonQuery();
                 }
             }
             catch (Exception e)
@@ -60,17 +84,50 @@ namespace ManateeConsole
                 TryDeleteImage(capture.FilePath);
                 throw new CaptureStorageException("Failed to add a capture to DB", e);
             }
+            finally
+            {
+                command.Dispose();
+            }
         }
 
         public CaptureEntry[] GetCaptures(DateTime time, int msMargin)
         {
             var from = time.ToUniversalTime().AddMilliseconds(-msMargin);
             var to = time.ToUniversalTime().AddMilliseconds(msMargin);
-            CaptureEntry[] captures;
-            lock (_dbLock)
+            List<CaptureEntry> captures = new List<CaptureEntry>();
+            var command = _db.CreateCommand();
+            command.CommandText =
+                string.Format(
+                    "SELECT Id, Timestamp, GateState, Source, FilePath FROM CaptureEntries WHERE Timestamp >= '{0}Z' AND Timestamp < '{1}Z'",
+                    from.ToString("yyyy-MM-dd HH:mm:ss.fffffff"), to.ToString("yyyy-MM-dd HH:mm:ss.fffffff"));
+            try
             {
-                captures = _db.CaptureEntries.Where(x => x.Timestamp >= @from && x.Timestamp < to).ToArray();
-                
+                lock (_dbLock)
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var capture = new CaptureEntry()
+                            {
+                                Id = reader.GetInt64(0),
+                                Timestamp = reader.GetDateTime(1),
+                                GateState = (GateState)reader.GetInt32(2),
+                                Source = (CaptureSource)reader.GetInt32(3),
+                                FilePath = reader.GetString(4)
+                            };
+                            captures.Add(capture);
+                        }    
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new CaptureStorageException("Failed to retrive captures from DB", e);
+            }
+            finally
+            {
+                command.Dispose();
             }
             return captures.GroupBy(x => x.Source, (source, entries) => entries.FirstOrDefault()).ToArray();
         }
@@ -175,27 +232,10 @@ namespace ManateeConsole
         }
     }
 
-    class CaptureStorageContext : DbContext
-    {
-        public DbSet<CaptureEntry> CaptureEntries { get; set; }
-
-        public CaptureStorageContext()
-            : base("CaptureStorageContext")
-        {
-            Database.Log = s => Debug.WriteLine(s);
-        }
-        
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            Database.SetInitializer(new SqliteCreateDatabaseIfNotExists<CaptureStorageContext>(modelBuilder));
-        }
-    }
-
     internal class CaptureEntry
     {
-        [Key, Autoincrement]
+        [Key]
         public long Id { get; set; }
-        [Index]
         public DateTime Timestamp { get; set; }
         public GateState GateState { get; set; }
         public CaptureSource Source { get; set; }
